5f0483632b51b8a804aac8fe6976bd393c5e86a0
YllkaNishevci <119699302+YllkaNishevci@users.noreply.github.com>
2023-11-28 18:21:03 +1100

    Commit1

---
+++ client.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+amespace MultiClient
+{
+    class Program
+    {
+        private static readonly Socket ClientSocket = new Socket
+            (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private const int PORT = 100;
+
+        static void Main()
+        {
+            Console.Title = "Client";
+            ConnectToServer();
+            RequestLoop();
+            Exit();
+        }
+    }
+}
